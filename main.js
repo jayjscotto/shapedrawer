a56c63712c31cffe4806bd2350978047e0c623fa
jayjscotto <jayjscotto@gmail.com>
2019-08-06 20:49:59 -0400

    added initial javascript

---
+++ main.js
@@ -0,0 +1,44 @@
+//
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+const btns = document.querySelectorAll('.btn');
+const shapes = {circle: 'circle', rectangle: 'rectangle' };
+let currentShape
+
+//circle drawing function
+const drawCircle = (mousex, mousey) => {
+    ctx.fillStyle = 'black';
+    ctx.beginPath();
+    ctx.arc(mousex, mousey, 10, 0, Math.PI * 2, false);
+    ctx.fill();
+    ctx.stroke();
+}
+
+//rectangle drawing function
+const drawRectangle = (mousex, mousey) => {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(mousex - 5, mousey - 10, 10, 20);
+}
+
+//for loop for each button that assigns currentShape variable to the shape data attribute
+for (let btn of btns) {
+    btn.addEventListener('click', e => {
+        currentShape = e.currentTarget.dataset.shape;
+        console.log(currentShape);
+        });
+}
+
+//event listener for the canvas, event handler will act based on value of currentShape
+canvas.addEventListener('click', e => {
+    // getting position of mouse
+    let rect = e.target.getBoundingClientRect();
+    let mousex = e.clientX - rect.left;
+    let mousey = e.clientY - rect.top;
+    console.log(mousex, mousey);
+
+    if (currentShape === shapes.circle) {
+        drawCircle(mousex, mousey);
+    } else if (currentShape === shapes.rectangle) {
+        drawRectangle(mousex, mousey);
+    }
+});
